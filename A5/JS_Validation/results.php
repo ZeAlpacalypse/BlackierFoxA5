b0eb9ef67f27268ab95f8a89feb8ec4cd2fdd3ff
Will Fox <foxwilliam358@gmail.com>
2023-11-09 22:17:09 -0400

    Js should be finished, needs testing

---
+++ A5/JS_Validation/results.php
@@ -0,0 +1,68 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>results</title>
+</head>
+
+<body>
+    <?php
+    class Question
+    {
+        private $questionID;
+        private $title;
+        private $choices;
+        private $answer;
+        private $points;
+        public function __construct($questionID, $title, $choices, $answer, $points)
+        {
+            $this->questionID = $questionID;
+            $this->title = $title;
+            $this->choices = $choices;
+            $this->answer = $answer;
+            $this->points = $points;
+
+        }
+        public function getQuestionID()
+        {
+            return $this->questionID;
+        }
+        public function getTitle()
+        {
+            return $this->title;
+        }
+        public function getChoices()
+        {
+            return $this->choices;
+        }
+        public function getAnswer()
+        {
+            return $this->answer;
+        }
+        public function getPoints()
+        {
+            return $this->points;
+        }
+        public function jsonSerialize()
+        {
+            return get_object_vars($this);
+        }
+    }
+    $id = $_POST["questionID"];
+    $qTitle = $_POST["title"];
+    $qAnswer = $_POST["answer"];
+    $qpoints = $_POST["points"];
+    $choiceAmt = $_POST["choices"];
+    $qChoices = [];
+    for ($i = 0; $i < intval($choiceAmt); $i++) {
+        $choice = $_POST["question" . $i];
+        array_push($qChoices, $choice);
+    }
+    $result = new Question($id, $qTitle, $qChoices, $qAnswer, $qpoints);
+    $jsonRes = json_encode($result);
+    echo $jsonRes;
+    ?>
+</body>
+
+</html>
